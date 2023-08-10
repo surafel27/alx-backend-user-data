7d6ad6d66cdcef4bf11736e0219631163f016d06
surafel27 <surafefekadu399@gmail.com.com>
2023-08-10 10:33:30 -0700

    compair the hashed password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -16,5 +16,5 @@ def hash_password(password: str) -> bytes:
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """Validates and checks a password against a hashed password."""
+    """Compair Validates and checks a password against the hashed password."""
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
