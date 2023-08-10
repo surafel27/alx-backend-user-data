72622039efe665516d766fdabf355f2a60ac62c2
surafel27 <surafefekadu399@gmail.com.com>
2023-08-10 10:31:39 -0700

    hash the password to never see the plain text

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+hash a password to never see the plain text
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    salted and hashed password will be returned, which is a byte string
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validates and checks a password against a hashed password."""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
