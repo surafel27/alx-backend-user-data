015bc60ef463aa79591b6766fbd1864172fdb804
surafel fekadu <surafelfekadu399@gmail.com>
2023-08-15 12:04:13 +0300

    empty session auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -18,7 +18,9 @@ auth = None
 if os.environ.get('AUTH_TYPE') == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-
+elif os.environ.get('AUTH_TYPE') == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 else:
     from api.v1.auth.auth import Auth
     auth = Auth()
--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 '''session auth class'''
 
-class 
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    '''sessionAuth class inherits Auth'''
+    pass
