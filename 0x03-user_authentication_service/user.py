821583f3f7d8be30f6961749a3f9d3be6a7ed57e
surafel27 <surafefekadu399@gmail.com.com>
2023-08-17 00:21:50 -0700

    User model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''user model using declaretive_base'''
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+
+class User(Base):
+    '''user model inherits from declarative_base'''
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
