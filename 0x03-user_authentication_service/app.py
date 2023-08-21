fb407bc01215ba3e8ea311350388914ea11c94f4
surafel27 <surafefekadu399@gmail.com.com>
2023-08-21 00:11:37 -0700

    Update password end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -105,7 +105,7 @@ def get_reset_password_token():
 
 
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
-def update_password():
+def update_password() -> str:
     """
     Update user's password via a PUT request to the /reset_password route.
     """
